1ec200d2d829cb8b37ca13d820f358c170998cdd
jbmsacps-stack <jbmsacps@gmail.com>
2026-07-25 16:43:48 +0530

    JB added generative grids for cartridge

---
+++ client/js/games.js
@@ -0,0 +1,38 @@
+const games = [
+
+{
+    title: "Super Mario",
+    year: 1985,
+    genre: "Platformer",
+    image: "assets/labels/mario.webp"
+},
+
+{
+    title: "Contra",
+    year: 1987,
+    genre: "Shooter",
+    image: "assets/labels/contra.webp"
+},
+
+{
+    title: "Pac-Man",
+    year: 1980,
+    genre: "Arcade",
+    image: "assets/labels/pacman.webp"
+},
+
+{
+    title: "Tetris",
+    year: 1984,
+    genre: "Puzzle",
+    image: "assets/labels/tetris.webp"
+},
+
+{
+    title: "Mega Man",
+    year: 1987,
+    genre: "Platformer",
+    image: "assets/labels/megaman.webp"
+}
+
+];
--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,41 @@
-const grid = document.querySelector(".game-grid");
+const grid = document.getElementById("gameGrid");
 
-for(let i=0;i<8;i++){
+games.forEach(game => {
 
     grid.innerHTML += `
 
-        <div class="game-card"></div>
+    <div class="game-card">
+
+        <div class="slot">
+            <img src="assets/cartridges/slot.svg">
+        </div>
+
+        <div class="cartridge">
+
+            <img class="label" src="${game.image}">
+
+            <img class="frame"
+                 src="assets/cartridges/retroholic-frame.svg">
+
+        </div>
+
+        <div class="pcb">
+            <img src="assets/cartridges/pcb.svg">
+        </div>
+
+        <div class="game-info">
+
+            <h3>${game.title}</h3>
+
+            <p>
+                ${game.genre}<br>
+                ${game.year}
+            </p>
+
+        </div>
+
+    </div>
 
     `;
 
-}
+});
