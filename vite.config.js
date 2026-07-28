35e96815ecf0ebd5ee4d72c92f1536c4a6b56371
jbmsacps-stack <jbmsacps@gmail.com>
2026-07-28 15:42:02 +0530

    JB did something with config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = resolve(__filename, "..");
 
 export default defineConfig({
     root: "client",
